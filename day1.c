cdd7db3c525eb23e3034f18f07ba244ea27edf74
Brandon Mejia <brandonandresmejia@gmail.com>
2018-12-03 13:36:56 -0500

    Day 1

---
+++ day1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    char c;
+    int f = 0;
+    int d = 0;
+    int n = 0;
+    while((c = getc(stdin)) != EOF){
+        #define INCR f+=d;d=0;
+        switch(c){
+            case '\n':
+            case '+': n = 0; INCR break;
+            case '-': n = 1; INCR break;
+            default: 
+                d = d * (d ? 10 : 1) + (n ? ('0' - c) : (c - '0'));
+        }
+    }
+    INCR
+
+    printf("Freq:%i\n", f);
+    
+}
